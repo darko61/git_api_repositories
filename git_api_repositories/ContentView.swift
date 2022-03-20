726a2cd7379616c3ce74a12f5e702bd0556c7c62
Darko <kamuflaza.golubovic@gmailcom>
2022-03-20 15:21:52 +0100

    Initial Commit

---
+++ git_api_repositories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  git_api_repositories
+//
+//  Created by Darko Golubovic on 20.3.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ git_api_repositories/git_api_repositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_api_repositoriesApp.swift
+//  git_api_repositories
+//
+//  Created by Darko Golubovic on 20.3.22..
+//
+
+import SwiftUI
+
+@main
+struct git_api_repositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
